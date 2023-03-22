bec86f56b8742cb4f55030b0b151d65ac921a33b
Habib-aw <habib.awahid01@gmail.com>
2023-03-22 14:34:15 +0000

    Add Ramadan images

--- RUN.py
+++ RUN.py
@@ -3,25 +3,47 @@ from tkinter import Tk,Label
 from Slide import *
 from Footer import *
 from Slideshow import *
-
+from hijri_converter import Gregorian
+from datetime import datetime
+from PIL import Image,ImageTk
 
 root = Tk()
 slideshow =Slideshow(root)
 f =Footer(root)
 
 s1 = Slide(root,
-title="Slide 1",
-content="Information goes here",
-contentFont=60
+title="",
+content="Baitul Ma'mur Academy",
+contentFont=212,
+paddingCtop=27.5
 )
-s2 = Slide(root,
-title="Slide 2",
-content="Other information goes here",
-contentFont=60
-)
+hijri = Gregorian(int(datetime.now().year), datetime.now().month, datetime.now().day).to_hijri()
+
+if hijri.month_name() !="Ramadhan":
+    try:
+        openedImage = Image.open("images/Ramadan/"+str(hijri.day)+".jpg")
+    except:
+        openedImage = Image.open("images/Ramadan/noImgFound.png")
+    width, height = openedImage.size
+    maxImgWidth = 1900
+    maxImgHeight=875
+    imgWidth = round((width/height)*maxImgHeight)
+    imgHeight = maxImgHeight
+    if imgWidth>maxImgWidth:
+        imgWidth=maxImgWidth
+        imgHeight=round((height/width)*maxImgWidth)
+    image = ImageTk.PhotoImage(openedImage.resize((imgWidth,imgHeight),Image.Resampling.LANCZOS))
+    ramadanMessage = Slide(root,None,image=image,title="")
+
+
 s1.packSlide()
 
-slideshow.addAll([s1,s2])
+slideshow.addAll([s1])
+try:
+    slideshow.add(ramadanMessage)
+except:
+    pass
+
 
 slideshow.redoTimes()
 root.config(bg=background)
